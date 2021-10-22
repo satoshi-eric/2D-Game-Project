acaffa8e425139452ec92259c2a321509a6d10ba
Eric Satoshi <satoshi.eric@gmail.com>
2021-10-22 19:17:33 -0300

    projeto feito

---
+++ Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public float DoubleJumpForce;
+    public bool isJumping;
+    public bool doubleJump;
+    public bool isFloating;
+
+    bool isBlowing;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        Float();
+        Exit();
+    }
+
+    private void Exit()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void Move()
+    {
+        // Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        // move personagem em uma posição
+        // transform.position += movement * Time.deltaTime * Speed;
+
+        float movement = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+
+        if(movement > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if (movement < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        if (movement == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && !isBlowing)
+        {
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else
+            {
+                if (doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, DoubleJumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+
+        }
+    }
+
+    void Float()
+    {
+        if (Input.GetButtonDown("Fly"))
+        {
+            if (isFloating)
+            {
+                rig.gravityScale = 4;
+                isFloating = false;
+            }
+            else
+            {
+                rig.gravityScale = 0;
+                isFloating = true;
+            }
+        }
+        if (isFloating)
+        {
+            Vector3 movement = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+            transform.position += movement * Time.deltaTime * Speed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            anim.SetBool("jump", false);
+        }
+        if (collision.gameObject.tag == "Spike")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "Saw")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 11)
+        {
+            isBlowing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 11)
+        {
+            isBlowing = false;
+        }
+    }
+}
